59c7b644cc406085d8e93a97232a54c32502a0a4
Marc Estevadeordal <emarc@pm.me>
2021-02-23 10:09:23 -0500

    testpackage

--- R/fbind.R
+++ R/fbind.R
@@ -1,3 +1,12 @@
+#' Conc Levels of Two Factors
+#'
+#' @param a Factor
+#' @param b Factor
+#'
+#' @return
+#' @export fbind
+#'
+#' @examples
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
